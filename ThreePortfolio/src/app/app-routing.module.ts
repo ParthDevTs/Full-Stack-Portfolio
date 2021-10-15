eebc5c07441f544dd3d4982f65cc665484e8eb55
Parth Kathuria <parth.kathuria99@gmail.com>
2021-10-15 11:40:00 +0530

    added blog and projects

--- ThreePortfolio/src/app/app-routing.module.ts
+++ ThreePortfolio/src/app/app-routing.module.ts
@@ -2,7 +2,18 @@ import { HomeComponent } from './modules/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'blog',
+    loadChildren: () => import('./blog/blog.module').then((m) => m.BlogModule),
+  },
+  {
+    path: 'project',
+    loadChildren: () =>
+      import('./project/project.module').then((m) => m.ProjectModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
