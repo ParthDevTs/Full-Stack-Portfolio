bd8d5d908a98a42ff99b264194c3d33996b2ca38
Parth Kathuria <parth.kathuria99@gmail.com>
2021-11-28 11:44:50 +0530

    Update animation.ts

--- ThreePortfolio/src/app/animation.ts
+++ ThreePortfolio/src/app/animation.ts
@@ -11,7 +11,7 @@ import { NONE_TYPE } from '@angular/compiler';
 
 export const slideInAnimation = trigger('routeAnimations', [
   transition('1 => 2,1=>4,2=>4', [
-    style({ height: '!' }),
+    style({ height: '!', overflow: 'hidden' }),
     query(':enter', style({ transform: 'translateX(100%)' })),
     query(
       ':enter, :leave',
@@ -36,7 +36,7 @@ export const slideInAnimation = trigger('routeAnimations', [
     ]),
   ]),
   transition('2 => 1,4=>2,4=>1', [
-    style({ height: '!' }),
+    style({ height: '!', overflow: 'hidden' }),
     query(':enter', style({ transform: 'translateX(-100%)' })),
     query(
       ':enter, :leave',
@@ -60,7 +60,7 @@ export const slideInAnimation = trigger('routeAnimations', [
       ),
     ]),
   ]),
-  transition('3 => 2, 3=>1,3=>4', [
+  transition('3<=>*', [
     style({ height: '!', overflow: 'hidden' }),
     query(':enter', style({ transform: 'translateY(-100%)' })),
     query(
@@ -91,38 +91,38 @@ export const slideInAnimation = trigger('routeAnimations', [
       ),
     ]),
   ]),
-  transition('4=>3 , 2 => 3 ,1=>3', [
-    style({ height: '!' }),
-    query(
-      ':enter',
-      style({ transform: 'translateY(100%)', overflow: 'hidden' })
-    ),
-    query(
-      ':enter, :leave',
-      style({
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        top: 0,
-        overflow: 'hidden',
-      })
-    ),
-    // animate the leave page away
-    group([
-      query(':leave', [
-        animate(
-          '0.8s cubic-bezier(.35,0,.25,1)',
-          style({ transform: 'translateY(-100%)', overflow: 'hidden' })
-        ),
-      ]),
-      // and now reveal the enter
-      query(
-        ':enter',
-        animate(
-          '0.8s cubic-bezier(.35,0,.25,1)',
-          style({ transform: 'translateY(0)', overflow: 'hidden' })
-        )
-      ),
-    ]),
-  ]),
+  // transition('4=>3 , 2 => 3 ,1=>3', [
+  //   style({ height: '!' }),
+  //   query(
+  //     ':enter',
+  //     style({ transform: 'translateY(100%)', overflow: 'hidden' })
+  //   ),
+  //   query(
+  //     ':enter, :leave',
+  //     style({
+  //       position: 'absolute',
+  //       left: 0,
+  //       right: 0,
+  //       top: 0,
+  //       overflow: 'hidden',
+  //     })
+  //   ),
+  //   // animate the leave page away
+  //   group([
+  //     query(':leave', [
+  //       animate(
+  //         '0.8s cubic-bezier(.35,0,.25,1)',
+  //         style({ transform: 'translateY(-100%)', overflow: 'hidden' })
+  //       ),
+  //     ]),
+  //     // and now reveal the enter
+  //     query(
+  //       ':enter',
+  //       animate(
+  //         '0.8s cubic-bezier(.35,0,.25,1)',
+  //         style({ transform: 'translateY(0)', overflow: 'hidden' })
+  //       )
+  //     ),
+  //   ]),
+  // ]),
 ]);
